87e5aa90b85d4931a67ff8f55afb488098007fce
HarleyVader <urukkai@outlook.com>
2025-01-08 14:39:03 +0100

    0.0.911

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-// config.js
 require('dotenv').config();
 
 module.exports = {
     clientId: process.env.PATREON_CLIENT_ID,
     clientSecret: process.env.PATREON_CLIENT_SECRET,
-    redirectUri: process.env.PATREON_REDIRECT_URI, // Ensure this matches your registered redirect URI
+    redirectUri: process.env.PATREON_REDIRECT_URI,
+    SESSION_SECRET: process.env.SESSION_SECRET, // Add this line
 };
-  
-
